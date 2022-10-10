1e1e1a016bbcd3278a9b058b325c2f90b7bfcef9
ttak0422 <ttak0422@gmail.com>
2022-10-10 13:35:00 +0900

    Add: neogen

--- flake.nix
+++ flake.nix
@@ -38,6 +38,10 @@
       url = "github:jubnzv/virtual-types.nvim";
       flake = false;
     };
+    neogen = {
+      url = "github:danymat/neogen";
+      flake = false;
+    };
   };
 
   outputs = { self, ... }@inputs: {
