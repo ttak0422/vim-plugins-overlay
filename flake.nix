46e2f63659099508a312dbcb3fdea0f51f61b5c5
ttak0422 <ttak0422@gmail.com>
2023-02-05 02:08:00 +0900

    Add: plugin

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    SmoothCursor-nvim = {
+      url = "github:gen740/SmoothCursor.nvim";
+      flake = false;
+    };
     statuscol-nvim = {
       url = "github:luukvbaal/statuscol.nvim";
       flake = false;
