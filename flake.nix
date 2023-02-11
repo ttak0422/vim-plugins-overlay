fb2d8ae8a5a3e2ea9a07d7a216f58788fbfbe31f
ttak0422 <ttak0422@gmail.com>
2023-02-11 19:22:06 +0900

    refactor

--- flake.nix
+++ flake.nix
@@ -867,7 +867,7 @@
         "x86_64-darwin"
         "aarch64-darwin"
       ];
-    in rec {
+    in {
       packages = eachSystem (system:
         let pkgs = import nixpkgs { inherit system; };
         in listToAttrs (map (name: {
