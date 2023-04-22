6a971d158b160971c2eba00bf1efd488c832307a
ttak0422 <ttak0422@gmail.com>
2023-04-22 22:26:25 +0900

    Revert "Revert "modify: plugin""
    This reverts commit 48ad5394dd78204cfc5eca8d0c3b0356896d3d22.

--- flake.nix
+++ flake.nix
@@ -1202,10 +1202,6 @@
       url = "github:troydm/zoomwintab.vim";
       flake = false;
     };
-    nvim-treesitter = {
-      url = "github:nvim-treesitter/nvim-treesitter";
-      flake = false;
-    };
     gitsigns-nvim = {
       url = "github:lewis6991/gitsigns.nvim";
       flake = false;
